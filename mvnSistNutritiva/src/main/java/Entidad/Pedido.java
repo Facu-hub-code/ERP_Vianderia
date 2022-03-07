37a9fd4309f0943aaf919d895b275ec2ab5bd7ec
Facu-hub-code <faculoren7@gmail.com>
2022-03-07 10:53:26 -0300

    mapeo pedidos entidad

--- mvnSistNutritiva/src/main/java/Entidad/Pedido.java
+++ mvnSistNutritiva/src/main/java/Entidad/Pedido.java
@@ -1,6 +1,7 @@
 package Entidad;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "pedidos")
@@ -9,9 +10,44 @@ public class Pedido {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idpedidos", nullable = false)
     private Integer id;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY) //todo: revisar el tefchtype
     @JoinColumn(name = "clientes_idclientes")
     private ClienteEntidad clientesIdclientes;
+
+    @Column(name = "fecha", nullable = false)
+    private LocalDate fecha;
+
+    @Lob
+    @Column(name = "tipo", nullable = false)
+    private String tipo;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "viandas_idviandas", nullable = false)
+    private ViandaEntidad viandasIdviandas;
+
+    public ViandaEntidad getViandasIdviandas() {
+        return viandasIdviandas;
+    }
+
+    public void setViandasIdviandas(ViandaEntidad viandasIdviandas) {
+        this.viandasIdviandas = viandasIdviandas;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
 
     public ClienteEntidad getClientesIdclientes() {
         return clientesIdclientes;
