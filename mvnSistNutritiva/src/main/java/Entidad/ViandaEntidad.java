b198f2930d9e20a82fc2e13a0fdb8dd8fc17fba5
Facu-hub-code <faculoren7@gmail.com>
2022-03-07 10:23:33 -0300

    mapeo vianda entidaad

---
+++ mvnSistNutritiva/src/main/java/Entidad/ViandaEntidad.java
@@ -0,0 +1,42 @@
+package Entidad;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "viandas")
+public class ViandaEntidad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idviandas", nullable = false)
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false, length = 45)
+    private String nombre;
+
+    @Column(name = "precio", nullable = false)
+    private Double precio;
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
