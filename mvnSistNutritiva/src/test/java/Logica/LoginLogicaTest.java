9b02ec93366d4afa4eb584f0a87abad1aa288c2d
Facu-hub-code <68453608+Facu-hub-code@users.noreply.github.com>
2022-03-31 19:44:00 -0300

    Test login logica

---
+++ mvnSistNutritiva/src/test/java/Logica/LoginLogicaTest.java
@@ -0,0 +1,18 @@
+package Logica;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class LoginLogicaTest {
+    LoginLogica loginLogica = new LoginLogica();
+
+    @Test
+    public void shouldAccesAllRight(){
+        Assertions.assertEquals(LoginLogica.validarIngreso("root", "toor"), true);
+    }
+
+}
