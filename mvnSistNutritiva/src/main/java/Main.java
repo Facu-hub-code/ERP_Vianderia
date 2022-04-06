138ceabb8cb9e8ac026adc4f7984215a06d98d59
Facu-hub-code <faculoren7@gmail.com>
2022-04-06 10:09:35 -0300

    Estructura progresiva de test

--- mvnSistNutritiva/src/main/java/Main.java
+++ mvnSistNutritiva/src/main/java/Main.java
@@ -1,21 +1,18 @@
 import Entidad.ClienteEntidad;
+import Entidad.PedidoEntidad;
+import Entidad.ViandaEntidad;
 import Interfaz.*;
+import Logica.ClienteLogica;
 import Logica.LoginLogica;
+import Logica.PedidoLogica;
+import Logica.ViandasLogica;
 
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.Calendar;
 
 public class Main {
-//    public static void main(String[] args) {
-//        new LoginInterfaz();
-//    }
 public static void main(String[] args) {
-    LoginLogica.validarIngreso("root", "toor");
-    //new SistemaPrincipalInterfaz();
-    //new ClientesInterfaz();
-    //new ViandasInterfaz();
-    //new PedidosInterfaz();
-    new TableroInterfaz();
-}
+    new LoginInterfaz();
+    }
 }
---
+++ mvnSistNutritiva/src/main/java/MainTest.java
@@ -0,0 +1,31 @@
+import Entidad.ClienteEntidad;
+import Entidad.PedidoEntidad;
+import Entidad.ViandaEntidad;
+import Interfaz.TableroInterfaz;
+import Logica.ClienteLogica;
+import Logica.LoginLogica;
+import Logica.PedidoLogica;
+import Logica.ViandasLogica;
+
+import java.sql.Date;
+
+public class MainTest {
+//    public static void main(String[] args) {
+//        new LoginInterfaz();
+//    }
+public static void main(String[] args) {
+    LoginLogica.validarIngreso("root", "toor");
+    //new SistemaPrincipalInterfaz();
+    //new ClientesInterfaz();
+    //new ViandasInterfaz();
+    //new PedidosInterfaz();
+    ViandaEntidad vianda = new ViandaEntidad("Vianda ejemplo", 200);
+    ViandasLogica.addVianda(vianda);
+    ClienteEntidad cliente = new ClienteEntidad("Facundo", "Lorenzo", "41411627",
+            "faculoren7@gmail.com", "3585098671", "Hipolito Yrigoyen 485");
+    ClienteLogica.addClient(cliente);
+    PedidoEntidad pedidoEntidad = new PedidoEntidad(cliente, vianda, Date.valueOf("2022-10-10"), "ALMUERZO");
+    PedidoLogica.addPedido(pedidoEntidad);
+    new TableroInterfaz();
+}
+}
