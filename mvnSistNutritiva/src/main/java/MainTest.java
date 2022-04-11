0d944844a67e7be383e6235599951b6019589e0e
Facu-hub-code <68453608+Facu-hub-code@users.noreply.github.com>
2022-04-11 16:24:59 -0300

    Test caja

--- mvnSistNutritiva/src/main/java/MainTest.java
+++ mvnSistNutritiva/src/main/java/MainTest.java
@@ -1,6 +1,4 @@
-import Entidad.ClienteEntidad;
-import Entidad.PedidoEntidad;
-import Entidad.ViandaEntidad;
+import Entidad.*;
 import Interfaz.CajaInterfaz;
 import Interfaz.SistemaPrincipalInterfaz;
 import Interfaz.TableroInterfaz;
@@ -9,15 +7,20 @@ import Logica.*;
 import Repository.PedidoRepository;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 public class MainTest {
     public static void main(String[] args) {
         LoginLogica.validarIngreso("root", "toor");
-        ViandaEntidad vianda = new ViandaEntidad("Vianda ejemplo", 200);
-        ViandasLogica.addVianda(vianda);
-        ClienteEntidad cliente = new ClienteEntidad("Facundo", "Lorenzo", "41411627",
-               "faculoren7@gmail.com", "3585098671", "Hipolito Yrigoyen 485");
-        ClienteLogica.addClient(cliente);
+        ViandasLogica.addVianda(new ViandaEntidad("Vianda ejemplo", 200));
+        ClienteLogica.addClient(new ClienteEntidad("Facundo", "Lorenzo", "41411627",
+                "faculoren7@gmail.com", "3585098671", "Hipolito Yrigoyen 485"));
+        PedidoLogica.addPedido(new PedidoEntidad(ClienteLogica.getClientes().get(0), ViandasLogica.getViandas().get(0),
+                Date.valueOf(LocalDate.now()), "ALMUERZO"));
+        CajaLogica.addMovimiento(new MovimientoEntidad(200, "venta", Date.valueOf(LocalDate.now()), true));
+        VentasLogica.addVenta(new VentaEntidad(Date.valueOf(LocalDate.now()), PedidoLogica.getPedidos().get(0),
+                CajaLogica.getMovimientos().get(0)));
+
         new CajaInterfaz();
     }
 }
